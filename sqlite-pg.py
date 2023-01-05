2d2c37bc78f0f97ad05bc28e7616c240a5838d8b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-05 19:54:42 +0530

    first commit

---
+++ sqlite-pg.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+connect = sqlite3.connect("data.db")
+
+# connect.execute("DROP TABLE IF EXISTS CUSTOMER;")
+# connect.execute(
+#     """
+#         CREATE TABLE CUSTOMER
+#         (ID INT PRIMARY KEY NOT NULL,
+#         NAME TEXT NOT NULL,
+#         AGE INT NOT NULL);
+#     """
+# )
+
+connect.execute("INSERT INTO CUSTOMER (ID, NAME, AGE) VALUES (1, 'NIRMALYA', 24);")
+connect.execute("INSERT INTO CUSTOMER (ID, NAME, AGE) VALUES (2, 'SAROJ', 25);")
+
+# connect.commit()
+
+all_data = connect.execute("""SELECT * FROM CUSTOMER;""")
+
+for row in all_data:
+    print(row)
+
+connect.close()
